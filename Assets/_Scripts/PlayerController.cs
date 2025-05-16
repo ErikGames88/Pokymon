6d7824ace1fd2e9b13f8776bc5ee0dfb166d40bf
ErikGames88 <erik.pan35@gmail.com>
2025-05-16 13:54:48 +0200

    Player Movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,13 +8,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private Vector2 input;
     private bool isMoving;
+    
 
 
     void Update()
     {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            // input.x = Input.GetAxisRaw("Horizontal");
+            // input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
+
+            if (input != Vector2.zero)
+            {
+                var targetPosition = transform.position;
+
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                StartCoroutine(MoveTowards(targetPosition));
+            }
+        }
+        
+        
     }
 
-    
+    IEnumerator MoveTowards(Vector3 destination)
+    {
+        isMoving = true;
+        while (Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+            yield return null;
+        }
+
+        transform.position = destination;
+        isMoving = false;
+    }
 }
